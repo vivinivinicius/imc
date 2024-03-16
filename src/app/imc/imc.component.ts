48db8446ee77d5fed9da6c0edb2ad31ff05552e7
Vinicius <legithacking069@gmail.com>
2024-03-16 20:36:21 -0300

    added bmi calculation

--- src/app/imc/imc.component.ts
+++ src/app/imc/imc.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class ImcComponent {
 
+peso: number = 0;
+altura: number = 0;
+resultado: number | null = null;
+
+calcularIMC() {
+  if (this.peso > 0 && this.altura > 0) {
+    this.resultado = this.peso / (this.altura * this.altura);
+
+  } else {
+    this.resultado = null;
+  
+  }
+}
+
 }
