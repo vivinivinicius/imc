a592e4645bd574ff2e624de856c553f3b5a244c2
Vinicius <legithacking069@gmail.com>
2024-03-16 20:33:47 -0300

    imc folder created

---
+++ src/app/imc/imc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imc',
+  templateUrl: './imc.component.html',
+  styleUrl: './imc.component.css'
+})
+export class ImcComponent {
+
+}
