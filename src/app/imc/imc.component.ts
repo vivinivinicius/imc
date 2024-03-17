57eb9bc786dc45eae8bdd036a7c7bd882063e623
Vinicius <legithacking069@gmail.com>
2024-03-16 21:04:33 -0300

    added images url

--- src/app/imc/imc.component.ts
+++ src/app/imc/imc.component.ts
@@ -19,16 +19,16 @@ calcularIMC() {
 
     if (this.resultado < 18.5) {
       this.classificacao = 'Abaixo do Peso';
-      this.imagem = '';
+      this.imagem = 'https://static.vecteezy.com/system/resources/previews/013/743/777/original/neutral-face-emoji-png.png';
     } else if (this.resultado >= 18.5 && this.resultado < 24.9) {
       this.classificacao = 'Peso Normal';
-      this.imagem = '';
+      this.imagem = 'https://png.pngtree.com/png-clipart/20221007/original/pngtree-happy-face-png-image_8663365.png';
     } else if (this.resultado >= 25 && this.resultado < 29.9) {
       this.classificacao = 'Sobrepeso';
-      this.imagem = '';
+      this.imagem = 'https://static.vecteezy.com/system/resources/previews/009/315/165/original/sad-3d-render-red-emoji-free-png.png';
     } else {
       this.classificacao = 'Obesidade';
-      this.imagem = '';
+      this.imagem = 'https://static.vecteezy.com/system/resources/previews/017/178/327/original/warning-hazard-sign-on-transparent-background-free-png.png';
     }
   } else {
     this.resultado = null;
